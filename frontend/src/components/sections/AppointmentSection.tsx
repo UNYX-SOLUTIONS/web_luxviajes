58be2a064a7c29f9572bfe69ecfd7fe727a2e2f3
yoston <yostoncornet@hotmail.es>
2026-04-27 09:57:49 -0500

    appontment

--- frontend/src/components/sections/AppointmentSection.tsx
+++ frontend/src/components/sections/AppointmentSection.tsx
@@ -590,7 +590,7 @@ export function AppointmentSection({
                   </p>
                 </div>
 
-                {/* <div>
+                <div>
                   <label className="block text-sm font-medium text-neutral-700 mb-2">
                     Mensaje
                   </label>
@@ -605,7 +605,7 @@ export function AppointmentSection({
                   <p className="text-xs text-neutral-400 text-right mt-1">
                     {formData.mensaje.length}/100
                   </p>
-                </div> */}
+                </div>
 
                 <div className="flex items-center">
                   <input
